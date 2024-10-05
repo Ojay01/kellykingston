9d11563d77ddd7f52f850f8d60152d32d019af3b
Kelly Kingston <okellykings220@gmail.com>
2024-10-05 07:08:11 +0100

    feat: service

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Intro from "@/components/homepage/intro.section";
 import ServicesSection from "@/components/homepage/services.section";
-import RecentWorksSection from "@/components/homepage/works.section";
+// import RecentWorksSection from "@/components/homepage/works.section";
 
 export default function Home() {
   return (
